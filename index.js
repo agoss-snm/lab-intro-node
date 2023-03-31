031758f7f7f65542c920351738be33e415d60e14
Agostina Romero <agostinaromontamatt@gmail.com>
2023-03-31 10:01:26 -0300

    solvedLab

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items= [],
+    this.length= 0;
+  }
 
-  add(item) {}
+ add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++;
+  }
 
-  get(pos) {}
 
-  max() {}
+   get(pos) {
 
-  min() {}
+    if( !this.items[pos]){
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos]
+  } 
 
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    const maxValue = Math.max(...this.items);
+    return maxValue;
+  }
+  
+  
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('OutOfBounds');
+    }
+    const minValue = Math.min(...this.items);
+    return minValue;
+  }
+  
+  
   sum() {}
 
   avg() {}
